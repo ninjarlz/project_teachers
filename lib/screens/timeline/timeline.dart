47a914802215998ec40b9f3e365f50c72a7d048e
Fabien <fabien.diaz@epitech.eu>
2020-05-11 23:06:57 +0200

    feat: Added searchBar on timeline

--- lib/screens/timeline/timeline.dart
+++ lib/screens/timeline/timeline.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:project_teachers/screens/timeline/post.dart';
 import 'package:project_teachers/widgets/index.dart';
+import 'package:project_teachers/themes/index.dart';
 
 class Timeline extends StatefulWidget {
+  static FloatingActionButton timelineFloatingActionButton(BuildContext context) {
+    return FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => TimelinePost()));
+        },
+        backgroundColor: ThemeGlobalColor().mainColor,
+        child: Icon(Icons.add));
+  }
+
   @override
   State<StatefulWidget> createState() => _TimelineState();
 }
 
 class _TimelineState extends State<Timeline> {
+  TextEditingController _searchCtrl = TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
+  void _searchFilter() {
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery
-          .of(context)
-          .size
-          .width,
+      width: MediaQuery.of(context).size.width,
       padding: EdgeInsets.all(10),
       child: Column(
         children: [
+          InputSearchWidget(
+            ctrl: _searchCtrl,
+            submitChange: _searchFilter,
+          ),
           Expanded(
             child: ListView.builder(
               itemCount: 3,
