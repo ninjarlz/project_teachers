4e06c3d5b2f81f319d104a95e94a2687f8327c4d
ninjarlz <michal.kusmidrowicz@gmail.com>
2020-06-28 00:34:12 +0200

    constants

---
+++ lib/utils/constants/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const String PROFILE_IMAGE_DIR = 'profile_image';
+  static const String BACKGROUND_IMAGE_DIR = 'background_image';
+  static const String USERS_DIR = "Users";
+  static const String QUESTIONS_DIR = "Questions";
+  static const String ANSWERS_DIR = "Answers";
+}
